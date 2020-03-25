7a751b706648f84d640e7fa744d526e1e4f456a1
Sahil Chawla <sahil.chawla@clutonik.ca>
2020-03-25 12:33:59 -0400

    Code to demonstrate zero Values in Go

--- src/main.go
+++ src/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"helloWorld"
+    "zeroValue"
 )
 
 func main() {
+    // Use helloWorld package to say Hello
     helloWorld.SayHello()
+
+    // Use zeroValue package to print default values for types
+    zeroValue.ZeroValue()
 }
