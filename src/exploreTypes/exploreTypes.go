1e1943a0a7012a390be7b5f15f693c31e56a3ff9
Sahil Chawla <sahil.chawla@clutonik.ca>
2020-03-25 23:43:11 -0400

    Code to create Custom Types in Go

--- src/exploreTypes/exploreTypes.go
+++ src/exploreTypes/exploreTypes.go
@@ -6,9 +6,19 @@ import (
 // Variable at global scope
 var z int = 4 // Recommended way of declaring a variable for global scope
 
+// It is possible to create your own TYPES in GO. 
+// When we create CUSTOM TYPE, package name is added to its name i.e. package_name.custom_type
+type id int
+
+var user_id id = 1 // user_id is a VARIABLE of CUSTOM TYPE exploreTypes.id and with value 1
+
 func PrintTypes() {
     a := "I think I am a string" // Variable at local scope (using short declaration operator recommended for local vars)
     fmt.Println("%T:",a) // This will not print the type 
     fmt.Printf("%T: %v\n",a,a) // This will replace %T with TYPE of var named 'a'
     fmt.Printf("%T: %v\n",z,z) // This will replace %T with TYPE of var named 'z'
+
+    // Custom TYPE
+    fmt.Printf("%T\t%v\n",user_id, user_id)
+
 }
